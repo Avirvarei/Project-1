f736ffd01541bcd422eeac0d47c45556602b1803
Avirvarei <Alexandru.avi@gmail.com>
2018-02-13 20:43:05 +0200

    Added simple script to get numbers from a file input.
    Change-Id: I452d510605b340e309b1e73f1e6fbf5cc115f1bc

---
+++ script1.py
@@ -0,0 +1,34 @@
+def get_numbers(list):
+    numbers = []
+    for element in list:
+        if type(element) == int or type(element) == float:
+            numbers.append(element)
+        if type(element) == str:
+            if only_numbers(element):
+                numbers.append(int(element))
+            else:
+                try:
+                    numbers.append(float(element))
+                except ValueError:
+                    pass
+    return numbers
+def only_numbers(line):
+    #returns True or False after checking if element is integer
+    for element in line:
+        if element not in "0123456789":
+            return False
+    return True
+def main(path):
+    #writes numbers in out.txt file
+    out = open("out.txt", "w")
+    with open(path) as file:
+        #gets numbers and writes them in out.txt file
+        lines = file.read().splitlines()
+        line = get_numbers(lines)
+        for item in line:
+            out.write("%s\n" % item)
+        out.close()
+if __name__ == "__main__":
+    #execute only if run as a script
+    path = input("Please write the path to file: ")
+    main(path)
---
+++ tests/test_script1.py
@@ -0,0 +1,60 @@
+from unittest import TestCase
+from scripts import main
+from scripts import get_numbers
+from code import characters
+
+
+class TestScript1(TestCase):
+    """
+    pass
+    """
+
+    def test_get_numbers(self):
+        """
+        Function should receive a list  and return only the numbers that are present in that list,  even if the
+        numbers are string.
+        :return:
+        """
+        self.assertEqual([1, 2, 3], get_numbers(["1", "2", "3"]))
+
+    def test_get_numbers_intercalated(self):
+        self.assertEqual([1, 2, 3], get_numbers(["1", "abc", "2", "abc2", "3"]))
+
+    def test_get_numbers_none(self):
+        self.assertEqual([], get_numbers(["abc", "abc2"]))
+
+    def test_get_numbers_ints(self):
+        self.assertEqual([1, 2, 3], get_numbers(["abc", "abc2", 1, 2, 3]))
+
+    def test_get_numbers_floats(self):
+        self.assertEqual([1.0, 2.0, 3], get_numbers(["abc", "1.0", "abc2", 2.0, 3]))
+
+
+class Test_Files(TestCase):
+    def assert_whatever(self, input, expected):
+        z = open("input.txt", "w")
+        for i in input:
+            z.write(str(i))
+            z.write("\n")
+        z.close()
+        main("input.txt")
+        o = []
+        with open("out.txt", "r") as l:
+            for line in l:
+                o.append(line.rstrip("\n"))
+        self.assertEqual(expected, o)
+
+    def test_get_numbers(self):
+        self.assert_whatever(["1", "2", "3"], ["1", "2", "3"])
+
+    def test_get_numbers_intercalated(self):
+        self.assert_whatever(["1", "abc", "2", "abc2", "3"], ["1", "2", "3"])
+
+    def test_get_numbers_none(self):
+        self.assert_whatever(["abc", "abc2"], [])
+
+    def test_get_numbers_ints(self):
+        self.assert_whatever(["abc", "abc2", 1, 2, 3], ["1", "2", "3"])
+
+    def test_get_numbers_floats(self):
+        self.assert_whatever(["abc", "1.0", "abc2", 2.0, 3], ["1.0", "2.0", "3"])
