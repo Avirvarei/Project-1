b09da75b4d2754ca9246143d3b3ea0f1d2414e48
Avirvarei <Alexandru.avi@gmail.com>
2018-02-21 19:21:20 +0200

    Added fizzbuzz script.
    Change-Id: I6b0fa63289e57211946e1627c8deb867c079902a

---
+++ fizz_buzz.py
@@ -0,0 +1,22 @@
+def fizz_buzz(number):
+    list = []
+    for i in range(1, number + 1):
+        if (i % 3 == 0) and (i % 5 == 0):
+            list.append("FizzBuzz")
+        elif (i % 3 == 0):
+            list.append("Fizz")
+        elif i % 5 == 0:
+            list.append("Buzz")
+        else:
+            list.append(str(i))
+    return ",".join(list)
+if __name__ == "__main__":
+    while True:
+        n = input("Please write a natural number: ")
+        # check if input is not number
+        if n == "exit":
+            exit(0)
+        try:
+            print(fizz_buzz(int(n)))
+        except ValueError:
+            print("This is not a natural number!")
